e42fc2e6cb43f6ca4c99e6cf58948540736c24c5
Omri Frazer <omrifrazer@Omris-MBP.attlocal.net>
2024-02-17 13:24:25 -0800

    Commplete model and index.js under models

---
+++ models/index.js
@@ -0,0 +1,39 @@
+// import models
+const Product = require('./Product');
+const Category = require('./Category');
+const Tag = require('./Tag');
+const ProductTag = require('./ProductTag');
+
+Product.belongsToMany(Category, {
+  // Define the third table needed to store the foreign keys
+  foreignKey:'category_id',
+  
+  onDelete:'CASCADE'
+});
+
+// Products belongsTo Category
+Category.hasMany(Product, {
+  foreignKey:'category_id',
+});
+
+// Categories have many Products
+
+// Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  foreignKey:'product_id',
+  through:ProductTag
+});
+
+// Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  foreignKey:'product_id',
+  through:ProductTag
+});
+
+
+module.exports = {
+  Product,
+  Category,
+  Tag,
+  ProductTag,
+};
